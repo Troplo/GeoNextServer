3f87718ce2d39511d3fe3b0e33b0245fba1abfa6
Troplo <troplo@troplo.com>
2025-10-23 02:46:33 +1100

    Update server.ts

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,7 +1,7 @@
 // SENTINEL SETTINGS
 
 // 5 minute kick timer
-export const KICK_IN_MS = 1 * 60 * 1000;
+export const KICK_IN_MS = 5 * 60 * 1000;
 // 48 hours
 export const ROOM_MAX_LIFE = 172_800_000;
 export const SENTINEL_SCAN_TIMEFRAME = 3000;
